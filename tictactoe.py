e040c22b94be9cc81af6c970d16284c5c658d634
boss hoss <judy.combs.reyes@gmail.com>
2019-09-02 01:43:14 -0500

    all work

---
+++ tictactoe.py
@@ -0,0 +1,89 @@
+#filename: tictactoe.py
+
+#############################################################
+
+#classes
+
+class Player:
+    def __init__(self, name, tokens):
+        self.tokens = tokens
+        self.name = name
+
+class Game:
+    def __init__(self):
+        self.board = [['_' for i in range(3)] for i in range(3)]
+    
+    def __repr__(self):
+        return f'{self.board}'
+
+    def move(self, x, y, player):
+        self.board[x][y] = player.tokens #if self.board[x][y] = '_' else print('cheater!')
+        
+    
+    def calc_winner(self):
+        if self.board[0] == self.board[1] == self.board[2]:
+            return 'start'
+        
+        elif self.board[0][0] == self.board[0][1] == self.board[0][2] and self.board[0][0] != '_':
+            return 'win'
+        elif self.board[1][0] == self.board[1][1] == self.board[1][2] and self.board[1][0] != '_':
+            return 'win'
+        elif self.board[2][0] == self.board[2][1] == self.board[2][2] and self.board[2][0] != '_':
+            return 'win'
+
+        elif self.board[0][0] == self.board[1][0] == self.board[2][0] and self.board[0][0] != '_':
+            return 'win' 
+        elif self.board[0][1] == self.board[1][1] == self.board[2][1] and self.board[0][1] != '_':
+            return 'win' 
+        elif self.board[0][2] == self.board[1][2] == self.board[2][2] and self.board[0][2] != '_':
+            return 'win'
+
+        elif self.board[0][0] == self.board[1][1] == self.board[2][2] and self.board[0][0] != '_':
+            return 'win' 
+        elif self.board[0][2] == self.board[1][1] == self.board[2][0] and self.board[0][2] != '_':
+            return 'win' 
+ 
+    
+    def isfull(self):
+        for l in self.board:
+            if '_' in l:
+                return True
+            else:
+                return False
+
+    def isgameover(self):
+        if self.isfull() == False or self.calc_winner() == 'win':
+            return True
+        else:
+            return False
+        
+        
+
+#############################################################
+
+def main():
+    
+    playerOne = Player(input('name: '), 'X')
+    playerTwo = Player(input('name: '), 'O')
+    players = [playerOne, playerTwo]
+
+    newgame = Game()
+    # print(newgame.__repr__())
+    # print(newgame.isfull())
+    # print(newgame.calc_winner())
+    # print(newgame.isgameover())
+
+    while newgame.isgameover() == False:
+        for player in players:
+                if newgame.isgameover() == False:
+                    newgame.move(int(input('row: ')),int(input('col: ')),player)
+                    print(newgame.__repr__())
+                    print(newgame.calc_winner())
+    if newgame.calc_winner() == 'win':
+        print(player,' is the winner!')
+    else:
+        print('draw...')
+        
+        
+        
+main()       
