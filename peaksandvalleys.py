5a2586deb2da1a41259560c03dbfade40459122a
boss hoss <judy.combs.reyes@gmail.com>
2019-09-05 03:45:40 -0500

    vers 3

--- peaksandvalleys.py
+++ peaksandvalleys.py
@@ -2,15 +2,17 @@
 
 data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 
-def findPeaks(data):
+def findPeaks(data): #able to recognize cliff at the end as peak
     peaks = []
     
-    for i in range(1, len(data)-1):
-
-        if data[i-1] < data[i] > data[i+1]:
-            peaks.append(i)
-
-    
+    for i in range(1, len(data)):
+        try:
+            if data[i-1] < data[i] > data[i+1]: 
+                peaks.append(i)
+        except:
+            if (data[i-1] < data[i] and i == len(data)-1):
+                peaks.append(i)
+   
     return peaks
     
 
@@ -37,17 +39,44 @@ def peakValley(peaks,valleys):
     return peaksAndValleys
 
 def drawMap(data):
-    for i in range(9,0,-1):
-        rowlist = []
+    listMap = []
+    for i in range(max(data),0,-1):
+        inlist = []
         for el in range(len(data)):
             if data[el] >= i:
-                rowlist.append('X')
+                inlist.append('X')
             else:
-                rowlist.append(' ')
-        print(' '.join(rowlist))      
+                inlist.append(' ')
+        listMap.append(inlist)
+    return listMap
+
+def gapFill(drawMap,findPeaks):
+    listMap = drawMap(data)
+    oCount = 0
+    for i in listMap:
+        for el in range(len(i)):
+            if i[el-1] in ['X','O'] and i[el] == ' ' and not el == 0:
+                i[el] = 'O'
+    for i in range(len(listMap)):
+        print(' '.join(listMap[i]),max(data)-i)
+    for i in listMap:
+        for el in i:
+            if el == 'O':
+                oCount += 1
+    print(oCount)
+    return None
+
+
+    
+            
+                
+
+    
+            
 
 print(peakValley(findPeaks(data),findValleys(data)))
-drawMap(data)
+
+gapFill(drawMap, findValleys)
 
 
 
